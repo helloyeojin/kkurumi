9be2743c3265356b46dfcd1d48dd54d8b8b6e6e5
mahanhur <allowbasmh@gmail.com>
2023-10-21 09:32:41 +0900

    cust dto 생성

---
+++ src/main/java/com/kkurumi/dto/Cust.java
@@ -0,0 +1,23 @@
+package com.kkurumi.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Cust {
+    private int user_pin;
+    private String user_name;
+    private String user_id;
+    private String gender;
+    private Date birthday;
+    private String address1;
+    private String address2;
+    private int job;
+    private int credit;
+}
